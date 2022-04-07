1760ad76f7cb09fcce6df3ebf937a1a09c76051f
Ismail <haaji.dheere@gmail.com>
2022-04-07 05:58:59 -0700

    update

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include <stdio.h>
-=======
 #include<stdio.h>
->>>>>>> ecc817a29ba57b88132477879a41a5bd10e852e3
-
 /**
  * main - Entry point
  *
@@ -12,8 +8,4 @@
 int main(void)
 {
     return (0);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ecc817a29ba57b88132477879a41a5bd10e852e3
