4f10bab2991a18b940d3a0532fb9800b0497ef56
Ismail Mohamed <haaj.dheere@gmail.com>
2022-04-07 16:03:35 +0300

    update

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,18 +1,5 @@
-#include <stdio.h>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-#include <stdio.h>
-=======
-=======
->>>>>>> 1760ad76f7cb09fcce6df3ebf937a1a09c76051f
 #include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
->>>>>>> 00e0b00e2a587c3a98c72de1defb4268a6cca94d
+
 int main(void)
 {
     return (0);
