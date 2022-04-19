8bdbd66efd001d310a84a09744b24382f4100037
Ismail Mohamed <haaji.dheere@gmail.com>
2022-04-19 21:13:46 +0300

    0-reset_to_98.c updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,5 @@
 #include "main.h"
-// Author By Eng Ismail
-// URL = https://github.com/macaanto14
-/**
- 
- * @n: An integer input pointer
- */
+// @n: An integer input pointer
 void reset_to_98(int *n)
 {
 	*n = 98;
