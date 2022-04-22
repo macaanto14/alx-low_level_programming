303300fc18afc12ab13b7cb5d9a16413ab1ed60b
Ismail Mohamed <haaji.dheere@gmail.com>
2022-04-22 21:31:46 +0300

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,11 @@
 
 /**
  * _strcat - a function that concatenates two strings.
+ * 
  * @dest: an input string
+ * 
  * @src: an input string
+ * 
  * Return: A pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
@@ -17,5 +20,6 @@ char *_strcat(char *dest, char *src)
 		*dest++ = *src++;
 
 	*dest = '\0';
+	
 	return (temp);
 }
