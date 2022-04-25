f1c1f762e5a01d0a0c049f6a5a3877e37efc26f7
Ismail Mohamed <haaji.dheere@gmail.com>
2022-04-25 01:50:02 -0700

    UPDATES

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,28 +1,18 @@
- /**
-  * Written By: Ismail Mohamed.
-  * Cohort_6 ALX-SE
-  */
-
-#include "main.h"
-
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
- *           to by @src into that pointed to by @dest.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
+ * _memcpy - copies n bytes from source memory to destination
  *
- * Return: A pointer to the destination buffer @dest.
+ * @dest: destination to write to
+ * @src: source to copy from
+ * @n: number of bytes to write
+ *
+ * Return: pointer to dest
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	char *ptr = dest;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	while (n--)
+		*ptr++ = *src++;
 
 	return (dest);
 }
-
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+#07. C - Even more pointers, arrays and strings
+## By Ismail Mohamed
+## ALX-Software Engineering Programm
+``` COHORT 6 ```
